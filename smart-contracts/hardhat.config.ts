28933174d517cb370a76734a16a41018586f9c6d
Ivo Spironello <99614711+aphxtwin@users.noreply.github.com>
2024-02-22 11:08:03 -0300

    Update hardhat.config.ts

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -1,8 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const ALCHEMY_API_KEY = "4_W7CkEwv6UBvTkXg6tMDoEbMqD4t5QD";
-const SEPOLIA_PRIVATE_KEY = "3b02f56eaa1524171fb627b1534c82420f0a2e6e1db77ab6c74adbfe7bd9176d"
+const ALCHEMY_API_KEY = process.env.ALCHEMY_KEY;
+const SEPOLIA_PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
